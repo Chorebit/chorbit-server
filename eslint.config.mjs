d492c5726d325553e6a1678cba424abba6fcc59d
Yaro <maksimenko.yr@gmail.com>
2026-04-08 23:46:14 +0300

    chore: set up ESLint + Prettier (#6)

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default defineConfig([
+  globalIgnores(['dist', 'node_modules']),
+  {
+    files: ['**/*.ts'],
+    extends: [js.configs.recommended, tseslint.configs.recommended, prettier],
+    languageOptions: {
+      ecmaVersion: 2020,
+    },
+  },
+]);
